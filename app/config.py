622bf83780b1f6ced0612e6b1298fd271c386ebe
Zakhar <lephiziel@gmail.com>
2026-02-19 18:21:12 +0500

    Update configuration file

--- app/config.py
+++ app/config.py
@@ -1,6 +1,14 @@
 from dotenv import load_dotenv
+from fastapi import FastAPI
 import os
 
 #database
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+#for JWT
+algorithm = os.getenv("ALGORITHM")
+secret_key = os.getenv("SECRET_KEY")
+
+#FastAPI app
+app = FastAPI()
