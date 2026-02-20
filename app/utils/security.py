2a53740c038ed9bd4143a721c90aeb955e90acf5
Zakhar <lephiziel@gmail.com>
2026-02-20 13:52:33 +0500

    Update architecture

---
+++ app/utils/security.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from app.config import algorithm, secret_key
+import os
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    if pwd_context.verify(plain_password, hashed_password):
+        return True
+    return False
+
+def create_access_token(data: dict) -> str:
+    token = jwt.encode(data, secret_key, algorithm=algorithm)
+    return token
+
+def decode_access_token(token: string) -> string:
+    try:
+        decoded_token = jwt.decode(token, secret_key, algorithms=algorithm)
+        return decoded_token["sub"]
+    except JWTError:
+        return None
