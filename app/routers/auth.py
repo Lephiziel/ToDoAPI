658fd87edcc3169bb95e5efc59299712e10de0d4
Zakhar <lephiziel@gmail.com>
2026-02-22 16:30:38 +0500

    Fix auth security problem

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -51,7 +51,7 @@ async def user_login(user: UserLogin,
 
     return Token(access_token=token, token_type="bearer")
 
-@router.get("/me")
+@router.get("/me", response_model=UserResponse)
 async def get_me(current_user: User = Depends(get_current_user)):
     return current_user
 
