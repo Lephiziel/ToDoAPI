f8a6967fb9d3ad460f5000e25a6efd600fc4dae9
Zakhar <lephiziel@gmail.com>
2026-02-22 16:32:10 +0500

    Connect ToDo routes to main file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.routers.auth import router
+from app.routers.todos import todo_router
 import uvicorn
 
 app = FastAPI()
 
 app.include_router(router, prefix="/api/auth", tags=["auth"])
+
+app.include_router(todo_router, prefix="/api/todo", tags=["todo"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
