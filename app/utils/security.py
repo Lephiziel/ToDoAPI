832353d70b30b3d6f20a6f1546217f3691f57fc5
Zakhar <lephiziel@gmail.com>
2026-02-23 22:20:41 +0500

    Improve the security.py

--- app/utils/security.py
+++ app/utils/security.py
@@ -13,17 +13,15 @@ def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    if pwd_context.verify(plain_password, hashed_password):
-        return True
-    return False
+    return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict) -> str:
     token = jwt.encode(data, secret_key, algorithm=algorithm)
     return token
 
-def decode_access_token(token: string) -> string:
+def decode_access_token(token: str) -> str | None:
     try:
-        decoded_token = jwt.decode(token, secret_key, algorithms=algorithm)
+        decoded_token = jwt.decode(token, secret_key, algorithms=[algorithm])
         return decoded_token["sub"]
     except JWTError:
         return None
