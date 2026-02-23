4a56f09ff6374a4fd551308299a469358dc310ab
Zakhar <lephiziel@gmail.com>
2026-02-23 22:21:08 +0500

    Improve the database.py

--- app/database.py
+++ app/database.py
@@ -5,12 +5,12 @@ from .config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
-sync_session_maker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 def get_db():
-    db = sync_session_maker()
+    db = SessionLocal()
     try:
         yield db
     finally:
